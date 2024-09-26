8593ad5e724beed4a0e7803173f913c14f7205ec
Peter Grapentien <a100933@qh623x3nf2.prog1.com>
2024-09-26 10:06:23 -0400

    Added core photo picker

--- funny-face-filter/funny-face-filter/PhotoPickerViewModel.swift
+++ funny-face-filter/funny-face-filter/PhotoPickerViewModel.swift
@@ -8,7 +8,40 @@
 import SwiftUI
 import PhotosUI
 
+struct Photo: Identifiable {
+    let id = UUID()
+    let image: UIImage
+}
+
+@MainActor
 class PhotoPickerViewModel: ObservableObject {
+    @Published var selectedPhoto: Photo?
+    @Published var pickerPhoto: PhotosPickerItem? {
+        didSet {
+            if let item = pickerPhoto {
+                loadPhoto(from: item)
+            }
+        }
+    }
+    
+    private func loadPhoto(from item: PhotosPickerItem) {
+        item.loadTransferable(type: Data.self, completionHandler: { result in
+            switch result {
+            case .success(let data):
+              if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                  self.selectPhoto(image)
+                }
+              }
+            case .failure(let error):
+              print("Error loading photo: \(error.localizedDescription)")
+            }
+        })
+    }
+    
+    func selectPhoto(_ photo: UIImage) {
+      selectedPhoto = Photo(image: photo)
+    }
 /*
  
     1.) Load photo from picker
